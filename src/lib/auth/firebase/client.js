c2cacb9b696defdcdda3cc3bb722f5e82589ccbe
DaRichardX <fishardoes@gmail.com>
2024-11-30 01:05:48 -0800

    debug, returns firebase env on startup

--- src/lib/auth/firebase/client.js
+++ src/lib/auth/firebase/client.js
@@ -1,9 +1,11 @@
 'use client';
 
 import { getAuth } from 'firebase/auth';
+import { config } from '@/config';
 
 import { getFirebaseApp } from '@/lib/firebase/client';
 
 export function getFirebaseAuth() {
+  console.log(config.firebase.apiKey);
   return getAuth(getFirebaseApp());
 }
