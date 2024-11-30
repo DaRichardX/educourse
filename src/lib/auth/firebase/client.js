0109a674ff92a3c4e95b11a823992c69b3253b11
DaRichardX <fishardoes@gmail.com>
2024-11-30 14:15:00 -0800

    Fixed public env not being passed to client at runtime cus next.js blackmajic

--- src/lib/auth/firebase/client.js
+++ src/lib/auth/firebase/client.js
@@ -6,6 +6,5 @@ import { config } from '@/config';
 import { getFirebaseApp } from '@/lib/firebase/client';
 
 export function getFirebaseAuth() {
-  console.log(config.firebase.apiKey);
   return getAuth(getFirebaseApp());
 }
